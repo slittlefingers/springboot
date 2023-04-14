3a5eae0b00b6152cb42524f1e7dc509f781c1952
dsha0062 <dsha0062@student.monash.edu>
2023-04-14 12:46:36 +0800

    sdf

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +15,9 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
+@EnableJpaRepositories(basePackages = "com.example.demo.repository") // 添加这一行
 @ComponentScan(basePackages = {"com.*"})
+@EntityScan(basePackages = {"com.example.demo.entity"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
